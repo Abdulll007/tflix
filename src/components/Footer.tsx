73ddbbaa360889b2d0ddcda51dcf32ebf6c16983
Abdulll007 <sadriwala.abdul@gmail.com>
2024-06-05 18:36:45 +0530

    footer content added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='h-12 w-full bg-[#222831] sm:px-64'></footer>
+    <footer className='h-12 w-full bg-[#222831] sm:px-64 text-white flex justify-center items-center text-lg'>
+      All Rights reserved &copy;
+    </footer>
   )
 }
 
