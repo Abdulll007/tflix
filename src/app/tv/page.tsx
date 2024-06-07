124cec0bbfd7c79becd15154574a33f671bd9931
Abdulll007 <sadriwala.abdul@gmail.com>
2024-06-07 20:26:20 +0530

    movie and tvshow page loading extra data issue fixed

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -9,58 +9,77 @@ import React, { useEffect, useState } from 'react'
 function Page() {
 
   const [tvShows, setTvShows] = useState<any[]>([])
-  const [currentPage, setCurrentPage] = useState(2) // Initialize to a number
+  const [currentPage, setCurrentPage] = useState(1) // Initialize to a number
 
   const fetchMoreData = async (pageNumber: number) => {
     const { data } = await axios.get(`${process.env.NEXT_PUBLIC_REQUEST_API}/tv/popular?page=${pageNumber}`, options)
     return data
   }
 
-  useEffect(()=>{
-    const firstFetch = async()=>{
+  useEffect(() => {
+    const firstFetch = async () => {
       const data = await fetchMoreData(1)
       setTvShows(data.results)
 
     }
-
     firstFetch()
-  },[])
+  }, [])
 
-  useEffect(() => {
-    const handleScroll = async () => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        try {
-          const response = await fetchMoreData(currentPage + 1)
-          const newTvData = response.results
-          setTvShows((prevTvShows) => [...prevTvShows, ...newTvData])
-          setCurrentPage((prevPage) => prevPage + 1) // Use functional update to ensure the correct state
-        } catch (error) {
-          console.log(error)
-        }
-      }
+  // useEffect(() => {
+  //   const handleScroll = async () => {
+  //     if (window.innerHeight + window.scrollY >= document.body.offsetHeight-100) {
+
+  //       try {
+
+  //         const response = await fetchMoreData(currentPage + 1)
+  //         const newTvData = response.results
+  //         setTvShows((prevTvShows) => [...prevTvShows, ...newTvData])
+  //         setCurrentPage((prevPage) => prevPage + 1) // Use functional update to ensure the correct state
+  //       } catch (error) {
+  //         console.log(error)
+  //       }
+  //     }
+  //   }
+
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => window.removeEventListener('scroll', handleScroll);
+
+  // }, [tvShows,currentPage]); // No need to depend on currentPage here
+
+  const loadMoreContent = async () => {
+    try {
+
+      const response = await fetchMoreData(currentPage + 1)
+      const newTvData = response.results
+      setTvShows((prevTvShows) => [...prevTvShows, ...newTvData])
+      setCurrentPage((prevPage) => prevPage + 1) // Use functional update to ensure the correct state
+    } catch (error) {
+      console.log(error)
     }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [currentPage]); // No need to depend on currentPage here
+  }
 
   return (
-    <div className='min-h-screen flex flex-wrap justify-center gap-4 text-white  sm:mx-20 md:mx-28'>
-      {tvShows?.map((shows) =>
+    <div className="flex flex-col items-center">
 
-        <Link href={`/tv/${shows?.id}`} key={shows.id}>
-        <Cards 
-          id={shows?.id}
-          name={shows?.name}
-          poster_path={shows.poster_path}
-          vote_average={shows.vote_average}
-          title={shows.title}
-          key={shows.id}
-          release_date={shows.release_date}
-          />
+      <div className='min-h-screen flex flex-wrap justify-center gap-4 text-white  sm:mx-20 md:mx-28'>
+        {tvShows?.map((shows, index) =>
+
+          <Link href={`/tv/${shows?.id}`} key={`${shows.id}/${index}`}>
+            <Cards
+              id={shows?.id}
+              name={shows?.name}
+              poster_path={shows.poster_path}
+              vote_average={shows.vote_average}
+              title={shows.title}
+              key={shows.id}
+              release_date={shows.release_date}
+            />
           </Link>
-      )}
+        )}
+
+      </div>
+      <button className="p-4 my-6 border text-white w-40 text-center rounded-lg text-xl" onClick={loadMoreContent}>Load more...</button>
     </div>
   )
 }
